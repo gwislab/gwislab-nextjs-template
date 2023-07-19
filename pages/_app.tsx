2f37a0fba0201dd368c2b3be02e04ac8a0226b74
cliffordten <cliffordteng5@gmail.com>
2023-07-19 14:51:57 +0100

    added appollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import React, { useEffect } from "react";
 
 import "assets/i18n";
 // import { IAppLanguage } from "configs";
+import { ApolloProvider } from "@apollo/client";
 import { useAppDispatch, useAppSelector } from "hooks";
+import useGraphqlClient from "hooks/useGraphqlClient";
 import { NextComponentType, NextPageContext } from "next";
 import type { AppProps } from "next/app";
 import { useTranslation } from "react-i18next";
@@ -17,13 +19,16 @@ interface ISubAppProps {
 const SubApp = ({ Component, ...pageProps }: ISubAppProps) => {
   const systemLanguage = useAppSelector((state) => state.language.systemLanguage);
   const dispatch = useAppDispatch();
+  const client = useGraphqlClient();
 
   useEffect(() => {
     dispatch(initializeLanguage(systemLanguage));
     return () => {};
   }, []);
   const { t, i18n } = useTranslation();
-  return <Component {...pageProps} t={t} i18n={i18n} />;
+  return     <ApolloProvider client={client}>
+    <Component {...pageProps} t={t} i18n={i18n} />;
+  </ApolloProvider>; 
 };
 
 export default function MyApp({ Component, pageProps }: AppProps) {
---
+++ src/hooks/useGraphqlClient.tsx
@@ -0,0 +1,21 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+import { useAppSelector } from "./redux";
+
+const useGraphqlClient = () => {
+  const token  = useAppSelector((state) => state.user);
+
+  console.log("in the client of graphql ");
+  const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql",
+    cache: new InMemoryCache(),
+    headers: {
+      "gwislab-user-locale": "en",
+      authorization: `Bearer ${token}`,
+    },
+  });
+
+  return client;
+};
+
+export default useGraphqlClient;
